c579d69dab76eb9af9a7a617767c180ee9c74dcf
unknown <alansolis707@gmail.com>
2025-10-31 08:20:29 -0600

    Programa que te permite mover sobre los ejes X y Y conforme el usuario le digite valores por medio de un menu de opciones en Orientada a Objetos

--- Poo.cpp
+++ Poo.cpp
@@ -1,29 +1,50 @@
-// Construir un programa que calcule el área y el perrímetro de un cuadrilátero dada la longitud de sus lados. Los valores 
-// de la longitud deberán introducirse por línea de ordenes. Si es un cuadrado, sólo se proporcionara la longitud de uno de los lados
-//al constructor.
+/*Construir un programa que permita dirigir el movimiento de un objeto dentro de un tablero y actualice su posicion
+dentro del mismo. Los movimientos posibles son ARRIBA, ABAJO, IZQUIERDA y DERECHA. Tras cada movimiento el programa
+mostrara la nueva direccion elegida y las coordenadas de situacion del objeto dentro del tablero. */
 #include <iostream>
-#include "Cuadrilatero.h"
+#include "Tablero.h"
 using namespace std;
 
 void modificar(Personaje &p,int at, int def){
-    Cuadrilatero* c1;
-    float lado1, lado2;
+    Tablero* ob1;
+    int x,y,opcion,n;
 
-    cout<<"Digite el lado 1: ";
-    cin>>lado1;
-    cout<<"Digite el lado 2: ";
-    cin>>lado2;
+    cout<<"Digite la posicion inicial del objeto: "<<endl;
+    cout<<"Posicion X: "; cin>>x;
+    cout<<"Posicion Y: "; cin>>y;
 
-    if(lado1==lado2){ //cuadrado
-        c1 = new Cuadrilatero(lado1);
-    }
-    else {
-         //rectangulo
-        c1 = new Cuadrilatero(lado1,lado2);
-    }
+    ob1 = new Tablero(x,y);
 
-    cout<<"El perimetro es: "<<c1->obtenerPerimetro()<<endl;
-    cout<<"El area es: "<<c1->obtenerArea()<<endl;
+    do{
+        cout<<"\n\t.:MENU:."<<endl;
+        cout<<"1. Arriba"<<endl;
+        cout<<"2. Abajo"<<endl;
+        cout<<"3. Derecha"<<endl;
+        cout<<"4. Izquierda"<<endl;
+        cout<<"5. Salir"<<endl;
+        cout<<"Opcion: ";
+        cin>>opcion;
+
+        if(opcion >= 1 && opcion <= 4){
+
+            cout<<"Digite la cantidad de espacios a moverse: ";
+            cin>>n;
+        }
+
+        switch(opcion){
+            case 1: ob1->moverArriba(n);
+                    break;
+            case 2: ob1->moverAbajo(n);
+                    break;
+            case 3: ob1->moverDerecha(n);
+                    break;
+            case 4: ob1->moverIzquierda(n);
+                    break;
+            case 5: break;
+            default: cout<<"\nSe equivoco de opcion de menu"<<endl;
+        }
+        cout<<"\nPosicion actual (X,Y): ("<<ob1->getX()<<","<<ob1->getY()<<")"<<endl;
+    }while(opcion != 5);
 
     return 0;
 }
